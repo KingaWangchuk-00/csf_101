f752fff1cf9ed6826da7a334f837c40eb9a52d82
unknown <iamkingaw630@gmail.com>
2024-05-09 11:40:01 +0530

    class

---
+++ exercises_csf101.py/Class.py
@@ -0,0 +1,18 @@
+class Dog:
+    def __init__(self,name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+dawa = Dog("Dawa", "white", 55)
+nima = Dog("nima", "colorless", 90)
+blacky = Dog("blacky", "black", 100)
+
+print(dawa.name)
+print(dawa.age)
+print(dawa.color)
+print(nima.name)
+print(nima.color)
+print(nima.age)
+print(blacky.name)
+print(blacky.age)
+print(blacky.color)
