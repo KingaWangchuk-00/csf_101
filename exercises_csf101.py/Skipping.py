134fb7982f65963e77f562b854da7284a9a28c15
unknown <iamkingaw630@gmail.com>
2024-05-09 11:52:43 +0530

    skipping

---
+++ exercises_csf101.py/Skipping.py
@@ -0,0 +1,17 @@
+for i in range(1,10):
+    print(i)
+    if i==3:
+        print("Skipping 3 in innner loop")
+        continue
+
+    if i==7:
+        print("Reached 7 breaking outer loop")
+        break
+    
+    
+
+
+
+        
+    
+    
